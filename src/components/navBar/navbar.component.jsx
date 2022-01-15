0dd6b7c6d714176f54913f84030da13bd30a5c6a
Juri <juriparladore@gmail.com>
2022-01-15 19:14:34 +0100

    deleted MODE

--- src/components/navBar/navbar.component.jsx
+++ src/components/navBar/navbar.component.jsx
@@ -5,7 +5,7 @@ export const NavBar = () => {
         <nav>
             <div className="nav">
                 <h1>Where in the world?</h1>
-                <span>MODE</span>
+                
             </div>
         </nav>
     )
